7b8ea7bed7fb069f7c51d058cdc4b32c5ad31c14
prasenjit-net <github@prasenjit.net>
2018-01-29 17:35:31 -0600

    test fixed

--- src/main/java/net/prasenjit/identity/controller/MeApi.java
+++ src/main/java/net/prasenjit/identity/controller/MeApi.java
@@ -4,6 +4,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Api(value = "ME", tags = "me", description = "A API which returns the user profile")
@@ -12,5 +14,5 @@ public interface MeApi {
     @ApiResponses(
             @ApiResponse(code = 200, message = "Success")
     )
-    UserDetails me();
+    UserDetails me(Authentication authentication);
 }
--- src/main/java/net/prasenjit/identity/controller/MeController.java
+++ src/main/java/net/prasenjit/identity/controller/MeController.java
@@ -1,19 +1,21 @@
 package net.prasenjit.identity.controller;
 
-import net.prasenjit.identity.doc.SwaggerDocumented;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import net.prasenjit.identity.doc.SwaggerDocumented;
 
 @RestController
 @SwaggerDocumented
 @RequestMapping(value = "api/me")
 public class MeController implements MeApi {
 
-    @Override
-    @GetMapping
-    public UserDetails me() {
-        return null;
-    }
+	@Override
+	@GetMapping
+	public UserDetails me(Authentication authentication) {
+		return (UserDetails) authentication.getPrincipal();
+	}
 }
