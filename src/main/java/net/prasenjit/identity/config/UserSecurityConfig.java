0756bf1432fa2d8cc7d393c055e617936bb4563b
prasenjit-net <github@prasenjit.net>
2018-07-05 14:32:56 -0500

    swagger ui correctly displays

--- src/main/java/net/prasenjit/identity/config/UserSecurityConfig.java
+++ src/main/java/net/prasenjit/identity/config/UserSecurityConfig.java
@@ -1,23 +1,33 @@
 package net.prasenjit.identity.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.CredentialsExpiredException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @Order(100)
 public class UserSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private static final AuthenticationHandler HANDLER = new AuthenticationHandler();
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/login", "/webjars/**")
+                .antMatchers("/login", "/webjars/**", "/swagger-resources/**")
                 .permitAll()
                 .mvcMatchers("/oauth/authorize")
                 .authenticated()
@@ -28,8 +38,47 @@ public class UserSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login")
+                .failureHandler(HANDLER)
+                .successHandler(HANDLER)
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(HANDLER)
                 .and()
                 .logout().and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
+    }
+
+    private static class AuthenticationHandler implements AuthenticationSuccessHandler, AuthenticationFailureHandler,
+            AuthenticationEntryPoint {
+        private static final String PREVIOUS_URL = AuthenticationHandler.class.getName() + ".PREVIOUS_URL";
+
+        @Override
+        public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                            AuthenticationException exception) throws IOException, ServletException {
+            if (exception instanceof CredentialsExpiredException) {
+                response.sendRedirect("/changePassword");
+            }else {
+                response.sendRedirect("/login?error");
+            }
+        }
+
+        @Override
+        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                            Authentication authentication) throws IOException, ServletException {
+            String requestURI = (String) request.getSession().getAttribute(PREVIOUS_URL);
+            if (requestURI != null) {
+                response.sendRedirect(requestURI);
+            } else {
+                response.sendRedirect("/");
+            }
+        }
+
+        @Override
+        public void commence(HttpServletRequest request, HttpServletResponse response,
+                             AuthenticationException authException) throws IOException, ServletException {
+            String requestURI = request.getRequestURI();
+            request.getSession().setAttribute(PREVIOUS_URL, requestURI);
+            response.sendRedirect("/login");
+        }
     }
 }
